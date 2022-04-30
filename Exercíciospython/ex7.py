e1a0502cad41f2fe7b1a11a50449f4c00047953f
João <joao.alexandress12@gmail.com>
2022-04-30 19:01:42 -0300

    Adiciona os comentários no exercício 7

--- Exercíciospython/ex7.py
+++ Exercíciospython/ex7.py
@@ -1,11 +1,14 @@
-num1 = int(input('Digite um número inteiro: '))
-num2 = int(input('Digite mais um número inteiro:'))
-num3 = float(input('Digite um número real: '))
+numero1 = int(input('Digite um número inteiro: '))
+numero2 = int(input('Digite mais um número inteiro:'))
+numero3 = float(input('Digite um número real: '))
 
-eq1 = (num1 * 2) * (num2 / 2)
-eq2 = num1 * 3 + num3
-eq3 = num3 * num3 * num3
+#Calcula o dobro do primeiro número vezes a metade do segundo numero
+equacao1 = (numero1 * 2) * (numero2 / 2)
+# Calcula o triplo do primeiro número somado ao terceiro número
+equacao2 = numero1 * 3 + numero3
+# Calcula o terceiro número elevado ao cubo
+equacao3 = numero3 * numero3 * numero3
 
 print('O produto do dobro do primeiro com metade do segundo é = {:.2f} \n'
       'A soma do triplo do primeiro com o terceiro é = {:.2f} \n'
-      'o terceiro elevado ao cubo é = {:.2f}'.format(eq1, eq2, eq3))
+      'o terceiro elevado ao cubo é = {:.2f}'.format(equacao1, equacao2, equacao3))
