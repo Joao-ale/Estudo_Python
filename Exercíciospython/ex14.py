f2705512cd3d175c32ded26e3a3722305498251f
João <joao.alexandress12@gmail.com>
2022-05-09 11:21:17 -0300

    Correção exercício para verificar o maior numero

--- Exercíciospython/ex14.py
+++
@@ -1,7 +0,0 @@
-num1 = int(input('Entre com um valor inteiro: '))
-num2 = int(input('Entre com outro valor inteiro: '))
-
-if num1 > num2:
-    print('O número {} é o maior entre os dois!'.format(num1))
-else:
-    print('O número {} é o maior entre os dois!'.format(num2))
